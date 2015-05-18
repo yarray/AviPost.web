2bb283b69d88c5f46c608e9dec5166d441624c88
yarray <08to09@gmail.com>
2015-05-18 19:33:15 +0200

    Add debug info for hooks

--- e2e/features/support/hooks.js
+++ e2e/features/support/hooks.js
@@ -1,11 +1,13 @@
-var clientSession = function () {
+var clientSession = function() {
     this.Before(function(callback) {
         this.client.init();
+        console.log('session init');
         callback();
     });
 
     this.After(function(callback) {
         this.client.end();
+        console.log('session end');
         callback();
     });
 };
