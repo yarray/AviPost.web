1445a53969457d11f91dc03b3b51faccdb3b3c59
yarray <08to09@gmail.com>
2015-06-12 01:55:10 +0200

    remove flow from gulp workflow since it still has some issues and will only be useful when running manually

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,6 @@ var myth = require('gulp-myth');
 var csso = require('gulp-csso');
 var serve = require('gulp-serve');
 var mocha = require('gulp-mocha');
-var flow = require('gulp-flowtype');
 
 // others
 var merge = require('merge-stream');
@@ -27,11 +26,6 @@ gulp.task('jshint', function() {
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'))
         .pipe(jshint.reporter('fail'));
-});
-
-gulp.task('flow', function() {
-    return gulp.src('./app/scripts/**/*.js')
-        .pipe(flow());
 });
 
 gulp.task('scripts', function() {
