e7baa73da2b71edaec1e7c248e6e9020d4a98319
yarray <08to09@gmail.com>
2015-06-20 01:32:50 +0200

    beautify

--- app/scripts/compose.js
+++ app/scripts/compose.js
@@ -35,7 +35,7 @@ var compose = function(root /* : Element */) {
     });
 
     preview.addEventListener('click', function(e) {
-        bg.classList.toggle('opaque');
+        bg.classList.toggle('white-trans');
         root.classList.toggle('foremost');
         e.target.classList.toggle('fa-eye-slash');
         if (!msgPanel.hasAttribute('disabled')) {
