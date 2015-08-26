4da21114e273f344830ebb3e418c66494c81d121
yarray <08to09@gmail.com>
2015-08-26 22:02:39 +0200

    switch to esformatter and eslint for better es6 support

--- app/scripts/gallery.js
+++ app/scripts/gallery.js
@@ -6,37 +6,38 @@ import { renderArray } from './template.js';
 import { removeNulls } from './helper.js';
 import { loadImage } from './async.js';
 
-
-// cannot declare as UI, because flow requires an interface file to handle
-// it, but it restart server every time and is super slow.
+/**
+ * @param {Element} root
+ * @param {string} uri
+*/
 function gallery(
-    root /*: Element */,
+    root /*: Element */ ,
     uri /*: string */
 ) {
     const container = root.querySelector('.page');
 
     const request = new XMLHttpRequest();
     request.open('GET', uri + '/postcards/');
-    request.setRequestHeader("Accept", "application/json");
+    request.setRequestHeader('Accept', 'application/json');
 
-    const cardElements /* : Promise<[Element]> */ = (
-        ajax.promise(request)
-        .catch(res => {
+    const cardElements /* : Promise<[Element]> */ = ajax.promise(request)
+        .catch(function(res) {
             console.error(res.statusText);
         })
         .then(res => {
             const data = JSON.parse(res.responseText);
             const cards = renderArray(data, container.querySelector('[data-template]'));
             return removeNulls(cards);
-        })
-    );
+        });
 
     // dom IO
-    cardElements.then(cards => {
+    cardElements
+        .then(cards => {
             cards.forEach(element => {
                 element.classList.add('hide');
                 container.appendChild(element);
             });
+
             return loadImage(container, cards);
         })
         .then(cards => {
