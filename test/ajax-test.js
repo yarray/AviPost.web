75d0e9ecf904e357c29f773bc9a5a2fae03741e1
yarray <08to09@gmail.com>
2015-09-24 01:13:30 +0200

    fix unit test; try to fix ci settings

--- test/ajax-test.js
+++ test/ajax-test.js
@@ -1,25 +1,47 @@
+import sinon from 'sinon';
+
 import ajax from '../app/scripts/ajax.js';
 
 describe('param parser', () => {
-    const params = ajax.params;
-
-    it('can parse simple params', () => {
-        expect(params({
-            a: 'hello',
-            b: 42,
-            c: 'world'
-        })).to.be.equal(
-            'a=hello&b=42&c=world'
-        );
+    beforeEach(function() {
+        this.xhr = sinon.useFakeXMLHttpRequest();
     });
 
-    it('can parse troublesome params', () => {
-        expect(params({
-            a: 'hell=',
-            b: '42&',
-            c: "'wor ld"
-        })).to.be.equal(
-            "a=hell%3D&b=42%26&c='wor%20ld"
-        );
+    afterEach(function() {
+        this.xhr.restore();
+    });
+
+    it('can parse simple params', function() {
+        ajax(
+            'http://127.0.0.1/', 'GET',
+            {
+                params: {
+                    a: 'hello',
+                    b: 42,
+                    c: 'world'
+                }
+            })
+            .then(() => {
+                expect(this.xhr.url).to.be.equal(
+                    'http://127.0.0.1/?a=hello&b=42&c=world'
+                );
+        });
+    });
+
+    it('can parse troublesome params', function() {
+        ajax(
+            'http://127.0.0.1/', 'GET',
+            {
+                params: {
+                    a: 'hell=',
+                    b: '42&',
+                    c: "'wor ld"
+                }
+            })
+            .then(() => {
+                expect(this.xhr.url).to.be.equal(
+                    "http://127.0.0.1/?a=hell%3D&b=42%26&c='wor%20ld"
+                );
+        });
     });
 });
