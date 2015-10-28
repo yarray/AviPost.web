eb155e402108d68a67f44acb4ce08581d025ec13
yarray <08to09@gmail.com>
2015-10-29 00:13:03 +0100

    use transparent border for nav; use cartodb tiles with retina support

--- app/scripts/worldmap.js
+++ app/scripts/worldmap.js
@@ -2,8 +2,11 @@ const L = require('leaflet');
 
 function worldmap(root) {
     const map = L.map(root).setView([51.3, 9.46], 10);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{retina}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+        subdomains: 'abcd',
+        maxZoom: 19,
+        retina: '@2x',
     }).addTo(map);
 }
 
